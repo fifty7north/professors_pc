fdc3a5e3b48e44ba1b9577217bd44534427f8f90
fifty7north <nathan.moir@outlook.com>
2023-12-04 20:39:38 +0000

    restructured pokemon data for variants

--- scripts/planner.js
+++ scripts/planner.js
@@ -17,8 +17,25 @@ const gameNumber = gameOrder.indexOf(selectedGame);
 
 const gamePokemonData = [];
 Object.entries(pokemonData).forEach(pokemon => {
-    if (pokemon[1].games_featured.includes(selectedGame)) {
-        gamePokemonData.push(pokemon);
+
+    //Sets a check if any of the variants of the pokemon feature in the selected game
+
+    let gameCheck = false;
+    pokemon[1].forEach(pokemonVariant => {
+        if (pokemonVariant['games_featured'].includes(selectedGame)) {
+            gameCheck = true;
+        }
+    })
+
+    //If any variant of the pokemon appears in the selected game, removes variants that do not appear in the game and pushes the remaining data to the gamePokemonData array 
+
+    if (gameCheck == true) {
+        for (let variant = 0; variant < pokemon[1].length; variant++) {
+            if (!pokemon[1][variant]['games_featured'].includes(selectedGame)) {
+                pokemon[1].splice(pokemon[1].indexOf(pokemon[1][variant]), 1)
+            }
+        }
+        gamePokemonData.push(pokemon)
     }
 });
 
@@ -27,48 +44,60 @@ Object.entries(pokemonData).forEach(pokemon => {
 //0 = pokemon has no weakness/resist to type, 1 = pokemon is weak to type, -1 = pokemon resists or is immune to type
 
 gamePokemonData.forEach(pokemon => {
+    for (let variant = 0; variant < pokemon[1].length; variant++) {
 
-    //Gets weakness/resistance data for each type the pokemon has
+        //Gets weakness/resistance data for each type the pokemon has
 
-    let pokemonWeaknessTypes = [];
-    pokemon[1]['type'].forEach(type => {
-        pokemonWeaknessTypes.push(typeData['weakness/resist_data'][type]);
-    })
+        let pokemonWeaknessTypes = [];
+        pokemon[1][variant]['type'].forEach(type => {
+            pokemonWeaknessTypes.push(typeData['weakness/resist_data'][type]);
+        })
 
-    //Combines the weakness data of each type the pokemon has
+        //Combines the weakness data of each type the pokemon has
 
-    let pokemonWeaknessArray = [];
-    for (let type = 0; type < pokemonWeaknessTypes[0].length; type++) {
-        let typeWeakness = 1;
-        for (let i = 0; i < pokemonWeaknessTypes.length; i++) {
-            typeWeakness = typeWeakness * pokemonWeaknessTypes[i][type];
+        let pokemonWeaknessArray = [];
+        for (let type = 0; type < pokemonWeaknessTypes[0].length; type++) {
+            let typeWeakness = 1;
+            for (let i = 0; i < pokemonWeaknessTypes.length; i++) {
+                typeWeakness = typeWeakness * pokemonWeaknessTypes[i][type];
+            }
+            pokemonWeaknessArray.push(typeWeakness);
         }
-        pokemonWeaknessArray.push(typeWeakness);
-    }
 
-    //Sets the values of each type in pokemonWeaknessArray to 0, 1 or -1 depending on the pokemon's weakness/resistance
+        //If the pokemon has an ability that modifies it's weaknesses, modifies specific type value based on ability
 
-    for (let type = 0; type < pokemonWeaknessArray.length; type++) {
-        if (pokemonWeaknessArray[type] > 1) {
-            pokemonWeaknessArray[type] = 1;
-        } else if (pokemonWeaknessArray[type] < 1) {
-            pokemonWeaknessArray[type] = -1;
-        } else {
-            pokemonWeaknessArray[type] = 0;
+        if (pokemon[1][variant]['ability'] !== 'default') {
+            if (pokemon[1][variant]['ability'] == 'heatproof') {
+                pokemonWeaknessArray[1] = pokemonWeaknessArray[1] * 0.5;
+            } else if (pokemon[1][variant]['ability'] == 'levitate') {
+                pokemonWeaknessArray[8] = 0;
+            }
         }
+
+        //Sets the values of each type in pokemonWeaknessArray to 0, 1 or -1 depending on the pokemon's weakness/resistance
+
+        for (let type = 0; type < pokemonWeaknessArray.length; type++) {
+            if (pokemonWeaknessArray[type] > 1) {
+                pokemonWeaknessArray[type] = 1;
+            } else if (pokemonWeaknessArray[type] < 1) {
+                pokemonWeaknessArray[type] = -1;
+            } else {
+                pokemonWeaknessArray[type] = 0;
+            }
+        }
+
+        //Removes dark/steel/fairy type data depending on game number (no dark/steel/fairy types in gen 1 games, no fairy type before gen 6 games)
+
+        if (gameNumber < 1) {
+            pokemonWeaknessArray.length = 15;
+        } else if (gameNumber < 8) {
+            pokemonWeaknessArray.length = 17;
+        }
+
+        //Adds the pokemonWeaknessArray to the pokemon's data
+
+        Object.assign(pokemon[1][variant], { 'type_weakness': pokemonWeaknessArray });
     }
-
-    //Removes dark/steel/fairy type data depending on game number (no dark/steel/fairy types in gen 1 games, no fairy type before gen 6 games)
-
-    if (gameNumber < 1) {
-        pokemonWeaknessArray.length = 15;
-    } else if (gameNumber < 8) {
-        pokemonWeaknessArray.length = 17;
-    }
-
-    //Adds the pokemonWeaknessArray to the pokemon's data
-
-    Object.assign(pokemon[1], {'type_weakness': pokemonWeaknessArray});
 });
 
 //For each pokemon in the chosen game, create an array containing all the pokemon's STAB type coverage
@@ -76,46 +105,48 @@ gamePokemonData.forEach(pokemon => {
 //0 = pokemon has no STAB coverage for type, 1 = pokemon has STAB coverage for type
 
 gamePokemonData.forEach(pokemon => {
+    for (let variant = 0; variant < pokemon[1].length; variant++) {
 
-    //Gets coverage data for each type the pokemon has
+        //Gets coverage data for each type the pokemon has
 
-    let pokemonCoverageTypes = [];
-    pokemon[1]['type'].forEach(type => {
-        pokemonCoverageTypes.push(typeData['coverage_data'][type]);
-    })
+        let pokemonCoverageTypes = [];
+        pokemon[1][variant]['type'].forEach(type => {
+            pokemonCoverageTypes.push(typeData['coverage_data'][type]);
+        })
 
-    //Combines the coverage data of each type the pokemon has
+        //Combines the coverage data of each type the pokemon has
 
-    let pokemonCoverageArray = [];
-    for (let type = 0; type < pokemonCoverageTypes[0].length; type++) {
-        let typeCoverage = 0;
-        for (let i = 0; i < pokemonCoverageTypes.length; i++) {
-            typeCoverage = typeCoverage + pokemonCoverageTypes[i][type];
+        let pokemonCoverageArray = [];
+        for (let type = 0; type < pokemonCoverageTypes[0].length; type++) {
+            let typeCoverage = 0;
+            for (let i = 0; i < pokemonCoverageTypes.length; i++) {
+                typeCoverage = typeCoverage + pokemonCoverageTypes[i][type];
+            }
+            pokemonCoverageArray.push(typeCoverage);
         }
-        pokemonCoverageArray.push(typeCoverage);
-    }
 
-    //Sets the values of each type in pokemonCoverageArray to 0 or 1 depending on the pokemon's has coverage for that type
+        //Sets the values of each type in pokemonCoverageArray to 0 or 1 depending on the pokemon's has coverage for that type
 
-    for (let type = 0; type < pokemonCoverageArray.length; type++) {
-        if (pokemonCoverageArray[type] > 0) {
-            pokemonCoverageArray[type] = 1;
-        } else {
-            pokemonCoverageArray[type] = 0;
+        for (let type = 0; type < pokemonCoverageArray.length; type++) {
+            if (pokemonCoverageArray[type] > 0) {
+                pokemonCoverageArray[type] = 1;
+            } else {
+                pokemonCoverageArray[type] = 0;
+            }
         }
+
+        //Removes dark/steel/fairy type data depending on game number (no dark/steel/fairy types in gen 1 games, no fairy type before gen 6 games)
+
+        if (gameNumber < 1) {
+            pokemonCoverageArray.length = 15;
+        } else if (gameNumber < 8) {
+            pokemonCoverageArray.length = 17;
+        }
+
+        //Adds the pokemonCoverageArray to the pokemon's data
+
+        Object.assign(pokemon[1][variant], { 'type_coverage': pokemonCoverageArray });
     }
-
-    //Removes dark/steel/fairy type data depending on game number (no dark/steel/fairy types in gen 1 games, no fairy type before gen 6 games)
-    
-    if (gameNumber < 1) {
-        pokemonCoverageArray.length = 15;
-    } else if (gameNumber < 8) {
-        pokemonCoverageArray.length = 17;
-    }
-
-    //Adds the pokemonCoverageArray to the pokemon's data
-
-    Object.assign(pokemon[1], {'type_coverage': pokemonCoverageArray});
 });
 
 /**
--- scripts/pokemon_data.js
+++ scripts/pokemon_data.js
@@ -1,106 +1,194 @@
 export default {
 
     /*
-    'pokemon name': {
-        'games_features': array of games where this pokemon features in the regional pokedex (rby, gsc, rbe, frlg, etc.)
-        'national_pokedex_id': id of pokemon in national pokedex
-        'regional_pokedex_id': array of id's of pokemon in regional pokedex, ordered by game order (rby, gsc, rbe, frlg, etc.), null if not pokemon not in game
-        'type': array of pokemon types (normal, fire, water, electric, etc.)
-        'base_stats': array of pokemon's base stats (hp, atk, def, sp. atk, sp. def, spd)
-        'trade_evolve': does pokemon evolve via trading, true/false
-        'starter': is the pokemon a starter pokemon, true/false
-        'legendary/mythic': is the pokemon a legendary/mythic pokemon, true/false
-    }
+    'pokemon name': [
+        New object in array for each variant (e.g. different forms, abilities that alter weaknesses, type change between games, etc.)
+        {
+            'games_features': array of games where this pokemon features in the regional pokedex (rby, gsc, rbe, frlg, etc.)
+            'national_pokedex_id': id of pokemon in national pokedex
+            'regional_pokedex_id': array of id's of pokemon in regional pokedex, ordered by game order (rby, gsc, rbe, frlg, etc.), null if not pokemon not in game
+            'type': array of pokemon types (normal, fire, water, electric, etc.)
+            'base_stats': array of pokemon's base stats (hp, atk, def, sp. atk, sp. def, spd)
+            'ability': 'default',
+            'trade_evolve': does pokemon evolve via trading, true/false
+            'starter': is the pokemon a starter pokemon, true/false
+            'legendary/mythic': is the pokemon a legendary/mythic pokemon, true/false
+        }
+    ]
     */
 
-    'venusaur': {
-        'games_featured': ['rby', 'frlg'],
-        'national_pokedex_id': 3,
-        'regional_pokedex_id': [3, null, null, 3],
-        'type': ['grass', 'poison'],
-        'base_stats': [80, 82, 83, 100, 100, 80],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'charizard': {
-        'games_featured': ['rby', 'frlg'],
-        'national_pokedex_id': 6,
-        'regional_pokedex_id': [6, null, null, 6],
-        'type': ['fire', 'flying'],
-        'base_stats': [78, 84, 78, 109, 85, 100],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'blastoise': {
-        'games_featured': ['rby', 'frlg'],
-        'national_pokedex_id': 9,
-        'regional_pokedex_id': [9, null, null, 9],
-        'type': ['water'],
-        'base_stats': [79, 83, 100, 85, 105, 78],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'meganium': {
-        'games_featured': ['gsc'],
-        'national_pokedex_id': 154,
-        'regional_pokedex_id': [null, 3, null, null],
-        'type': ['grass'],
-        'base_stats': [80, 82, 100, 83, 100, 80],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'typhlosion': {
-        'games_featured': ['gsc'],
-        'national_pokedex_id': 157,
-        'regional_pokedex_id': [null, 6, null, null],
-        'type': ['fire'],
-        'base_stats': [78, 84, 78, 109, 85, 100],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'feraligatr': {
-        'games_featured': ['gsc'],
-        'national_pokedex_id': 160,
-        'regional_pokedex_id': [null, 9, null, null],
-        'type': ['water'],
-        'base_stats': [85, 105, 100, 79, 83, 78],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'sceptile': {
-        'games_featured': ['rbe'],
-        'national_pokedex_id': 254,
-        'regional_pokedex_id': [null, null, 3, null],
-        'type': ['grass'],
-        'base_stats': [70, 85, 65, 105, 85, 120],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'blaziken': {
-        'games_featured': ['rbe'],
-        'national_pokedex_id': 257,
-        'regional_pokedex_id': [null, null, 6, null],
-        'type': ['fire', 'fighting'],
-        'base_stats': [80, 120, 70, 110, 70, 80],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
-    'swampert': {
-        'games_featured': ['rbe'],
-        'national_pokedex_id': 260,
-        'regional_pokedex_id': [null, null, 9, null],
-        'type': ['water', 'ground'],
-        'base_stats': [100, 110, 90, 85, 90, 60],
-        'trade_evolve': false,
-        'starter': true,
-        'legendary/mythic': false
-    },
+    'venusaur': [
+        {
+            'games_featured': ['rby', 'frlg'],
+            'national_pokedex_id': 3,
+            'regional_pokedex_id': [3, null, null, 3, null],
+            'type': ['grass', 'poison'],
+            'base_stats': [80, 82, 83, 100, 100, 80],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        },
+        {
+            'games_featured': ['gsc', 'hgss', 'xy'],
+            'national_pokedex_id': 3,
+            'regional_pokedex_id': [3, null, null, 3, null],
+            'type': ['grass', 'poison'],
+            'base_stats': [80, 82, 83, 100, 100, 80],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'charizard': [
+        {
+            'games_featured': ['rby', 'frlg'],
+            'national_pokedex_id': 6,
+            'regional_pokedex_id': [6, null, null, 6, null],
+            'type': ['fire', 'flying'],
+            'base_stats': [78, 84, 78, 109, 85, 100],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        },
+        {
+            'games_featured': ['gsc', 'hgss', 'xy'],
+            'national_pokedex_id': 6,
+            'regional_pokedex_id': [6, null, null, 6, null],
+            'type': ['fire', 'flying'],
+            'base_stats': [78, 84, 78, 109, 85, 100],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'blastoise': [
+        {
+            'games_featured': ['rby', 'frlg'],
+            'national_pokedex_id': 9,
+            'regional_pokedex_id': [9, null, null, 9, null],
+            'type': ['water'],
+            'base_stats': [79, 83, 100, 85, 105, 78],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        },
+        {
+            'games_featured': ['gsc', 'hgss', 'xy'],
+            'national_pokedex_id': 9,
+            'regional_pokedex_id': [9, null, null, 9, null],
+            'type': ['water'],
+            'base_stats': [79, 83, 100, 85, 105, 78],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'meganium': [
+        {
+            'games_featured': ['gsc'],
+            'national_pokedex_id': 154,
+            'regional_pokedex_id': [null, 3, null, null, null],
+            'type': ['grass'],
+            'base_stats': [80, 82, 100, 83, 100, 80],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'typhlosion': [
+        {
+            'games_featured': ['gsc'],
+            'national_pokedex_id': 157,
+            'regional_pokedex_id': [null, 6, null, null, null],
+            'type': ['fire'],
+            'base_stats': [78, 84, 78, 109, 85, 100],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'feraligatr': [
+        {
+            'games_featured': ['gsc'],
+            'national_pokedex_id': 160,
+            'regional_pokedex_id': [null, 9, null, null, null],
+            'type': ['water'],
+            'base_stats': [85, 105, 100, 79, 83, 78],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'sceptile': [
+        {
+            'games_featured': ['rbe'],
+            'national_pokedex_id': 254,
+            'regional_pokedex_id': [null, null, 3, null, null],
+            'type': ['grass'],
+            'base_stats': [70, 85, 65, 105, 85, 120],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'blaziken': [
+        {
+            'games_featured': ['rbe'],
+            'national_pokedex_id': 257,
+            'regional_pokedex_id': [null, null, 6, null, null],
+            'type': ['fire', 'fighting'],
+            'base_stats': [80, 120, 70, 110, 70, 80],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'swampert': [
+        {
+            'games_featured': ['rbe'],
+            'national_pokedex_id': 260,
+            'regional_pokedex_id': [null, null, 9, null, null],
+            'type': ['water', 'ground'],
+            'base_stats': [100, 110, 90, 85, 90, 60],
+            'ability': 'default',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ],
+    'bronzong': [
+        {
+            'games_featured': ['dppt'],
+            'national_pokedex_id': 437,
+            'regional_pokedex_id': [null, null, null, null, 89],
+            'type': ['steel', 'psychic'],
+            'base_stats': [67, 89, 116, 79, 116, 33],
+            'ability': 'levitate',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        },
+        {
+            'games_featured': ['dppt'],
+            'national_pokedex_id': 437,
+            'regional_pokedex_id': [null, null, null, null, 89],
+            'type': ['steel', 'psychic'],
+            'base_stats': [67, 89, 116, 79, 116, 33],
+            'ability': 'heatproof',
+            'trade_evolve': false,
+            'starter': true,
+            'legendary/mythic': false
+        }
+    ]
 }
